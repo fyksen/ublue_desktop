d228b1e3cf3fac6cfa78788f898890be4f38f240
fyksen <fredrik@fyksen.me>
2024-09-02 15:25:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen ncdu
+rpm-ostree install screen ncdu qemu-guest-agent
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
@@ -21,3 +21,4 @@ rpm-ostree install screen ncdu
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable qemu-guest-agent.service
